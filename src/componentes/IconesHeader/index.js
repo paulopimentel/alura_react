d26e39b4e0add6440b7b4b56f9e910516a59bac1
Paulo Pimentel <paulo.santos@iob.com.br>
2023-02-07 22:12:35 -0300

    First Commit

---
+++ src/componentes/IconesHeader/index.js
@@ -0,0 +1,30 @@
+import perfil from '../../imagens/perfil.svg'
+import sacola from '../../imagens/sacola.svg'
+import styled from 'styled-components'
+
+const Icone = styled.li
+`
+  margin-right: 40px;
+  width: 25px;
+`
+const Icones = styled.ul 
+`
+  display: flex;
+`
+
+const icones = [perfil, sacola]
+
+function IconesHeader() {
+    return(
+        <Icones>
+          {
+            icones.map( (icone) => 
+            (
+              <Icone><img src={icone} alt='icone'></img></Icone>
+            ) )
+          }
+        </Icones>
+    )
+}
+
+export default IconesHeader
---
+++ src/componentes/UltimosLancamentos/index.js
@@ -0,0 +1,54 @@
+import { livros } from './dadosUltimosLancamentos'
+import styled from 'styled-components'
+import { Titulo } from '../Titulo'
+import CardRecomenda from '../CardRecomenda'
+import imagemLivro from '../../imagens/livro2.png'
+
+const UltimosLancamentosContainer = styled.section`
+    background-color: #EBECEE;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+`
+
+const NovosLivrosContainer = styled.div`
+    margin-top: 30px;
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    cursor: pointer;
+`
+
+function UltimosLancamentos(){
+    return(
+        <UltimosLancamentosContainer>
+            <Titulo 
+                cor="#EB9B00"
+                tamanhoFonte="36px"
+            >
+                ÚLTIMOS LANÇAMENTOS
+            </Titulo>
+            <NovosLivrosContainer>
+            { livros.map( livro => (
+                <img src={livro.src} alt='livro' />
+            ) )}
+            </NovosLivrosContainer>
+
+            <CardRecomenda
+                titulo = "Talvez você se interesse por..."
+                subtitulo = "Angular 11"
+                descricao = "Construindo uma aplicação integrada"
+                img = {imagemLivro}
+            />
+            <p></p>
+            <CardRecomenda
+                titulo = "Talvez você se interesse por..."
+                subtitulo = "Angular 12"
+                descricao = "Construindo uma aplicação integrada"
+                img = {imagemLivro}
+            />
+
+        </UltimosLancamentosContainer>
+    )
+}
+export default UltimosLancamentos
